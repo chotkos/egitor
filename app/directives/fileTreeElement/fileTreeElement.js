d7a94a662121da2f3727a5d2a1124d79f928c48f
Mateusz Chodkowski <Mateusz.Chodkowski@promise.pl>
2017-04-13 16:00:28 +0200

    active element

--- app/directives/fileTreeElement/fileTreeElement.js
+++ app/directives/fileTreeElement/fileTreeElement.js
@@ -1,41 +1,64 @@
-app.directive("fileTreeElement", function($compile) {
+app.directive("fileTreeElement", function ($compile) {
     return {
-        restrict: 'E', 
-        link: function(scope){},
-        compile:function(tElement,tAttr){
+        restrict: 'E',
+        link: function (scope) {},
+        compile: function (tElement, tAttr) {
             var contents = tElement.contents().remove();
             var compiledContents;
-            return function(scope, iElement, iAttr) {
-                if(!compiledContents) {
+            return function (scope, iElement, iAttr) {
+                if (!compiledContents) {
                     compiledContents = $compile(contents);
                 }
-                compiledContents(scope, function(clone, scope) {
-                         iElement.append(clone); 
+                compiledContents(scope, function (clone, scope) {
+                    iElement.append(clone);
                 });
             };
         },
-        controller: function($scope,$location){
-            var guid = function() {
+        controller: function ($scope, $location) {
+            var guid = function () {
                 function s4() {
                     return Math.floor((1 + Math.random()) * 0x10000)
-                    .toString(16)
-                    .substring(1);
+                        .toString(16)
+                        .substring(1);
                 }
                 return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
                     s4() + '-' + s4() + s4() + s4();
             };
 
-            $scope.guid = guid();         
-            $scope.clickedTreeElement = function(el){
-                if(el.extension){ 
-                    $location.path('file').search({path: el.path, name:el.name});       
+            $scope.guid = guid();
+            $scope.clickedTreeElement = function (el) {
+                if (el.extension) {
+                    $location.path('file').search({
+                        path: el.path,
+                        name: el.name
+                    });
                 }
-            }   
+
+                var getByScopeId = function (id) {
+                    var filterfn = function (i, el) {
+                        var sc = angular.element(el).scope();
+
+                        return sc && sc.$id == id;
+                    };
+                    // low hanging fruit -- actual scope containers
+                    var result = $('.ng-scope').filter(filterfn);
+                    if (result && result.length) return result;
+
+                    // try again on everything...ugh
+                    return $(':not(.ng-scope)').filter(filterfn);
+                }
+                if (el.extension) {
+                    var element = getByScopeId(this.$id);
+                    console.log(element[0]);
+                    $('.active-file').removeClass("active-file");
+                    $(element[0]).addClass("active-file");
+                }
+            }
         },
         scope: {
-            child: '=' 
+            child: '='
         },
-        transclude:true,
-        templateUrl : "app/directives/fileTreeElement/fileTreeElement.html"
+        transclude: true,
+        templateUrl: "app/directives/fileTreeElement/fileTreeElement.html"
     };
 });
